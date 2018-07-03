920b23755ab65d6559b7a2843ce0e218f68a144e
Jan Ruzicka <jan-ruzicka@users.noreply.github.com>
2018-07-02 23:30:01 -0400

    use central config instead of local settings

--- init-postgresql.bat
+++ init-postgresql.bat
@@ -2,50 +2,7 @@
 @echo off
 REM . `dirname $0`/demo.conf
 
-
-set ADMIN_USER=admin
-set ADMIN_PASS="admin1jboss!"
-
-set VER_DIST_EAP=7.1.0
-set VER_INST_EAP=7.1
-set VER_PATCH_EAP=7.1.3
-
-set PGJDBC=postgresql-42.2.2.jar
-REM set PGDATA=/usr/local/var/postgres
-set PG_HOME=D:\PostgreSQL\10
-set PGDATA=%PG_HOME%\data
-set PGSERV=postgresql-x64-10
-set PGDBNAME=managezoo
-set PGUSER=manager
-set PGPASS=manager
-set PGJNDI=ManageZooDS
-
-REM function echo     OK {
-REM   if [[ $? -eq 0 ]]
-REM   then
-REM     echo "[OK]"
-REM   else
-REM     echo "[FAILED]"
-REM   fi
-REM }
-
-REM function PUSHD {
-REM   pushd $1 &> /dev/null
-REM }
-
-REM function POPD {
-REM   popd &> /dev/null
-REM }
-
-REM PUSHD $(dirname $0)
-REM WORKDIR=$(pwd)
-REM POPD
-set WORKDIR=D:\GitProjects\fun-with-forge
-
-set BINDIR=%WORKDIR%\dist
-set JBOSS_HOME=%WORKDIR%\jboss-eap-%VER_INST_EAP%
-
-
+call demo.conf.bat
 
 echo Do not use.
 echo Re-initialization of the database is not correct
